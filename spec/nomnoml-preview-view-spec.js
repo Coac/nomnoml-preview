07a031bb5cdeb452d68c5830c9f2cfd7d51887eb
Coac <levictor3000@hotmail.fr>
2016-10-29 14:08:27 +0200

    Initial package files

---
+++ spec/nomnoml-preview-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import NomnomlPreviewView from '../lib/nomnoml-preview-view';
+
+describe('NomnomlPreviewView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
